c6d0360f1b5659747bf528c68705c2cee3d9f4fb
Ibrahim <ibrahim.hassan2402@gmail.com>
2024-09-26 13:26:54 +0100

    initial commit, task one done

---
+++ NumberGame.js
@@ -0,0 +1,63 @@
+function generateRandomNumber() {
+    return Math.floor((Math.random() * 100))
+}
+
+const getPlayerGuess = function(){
+    let x = prompt("Guess a number between 1 and 100.\n")
+    x = Number(x)
+    
+    if(Number.isInteger(x)){
+        return x;
+    } else {
+        console.log("invalid input\n");
+        return getPlayerGuess();
+    }
+}
+
+const checkGuess = (pg, ca) => {
+    if(pg>ca){
+        console.log("too high\n");
+        return false;
+    } else if(pg<ca){
+        console.log("too low\n");
+        return false;
+    } else{
+        return true;
+    }
+}
+
+function game(){
+    
+    let number = generateRandomNumber();
+    let guesses = [];
+    let state = false;
+    
+    
+    for(let i=0;i<10;i++){
+        let guess = getPlayerGuess();
+        guesses.push(guess);
+        console.log("guesses: ", guesses);
+        
+        if(checkGuess(guess,number)){
+            state = true;
+            break
+        }
+    }
+    
+    if(state){
+        console.log("YOU WIN!\n");
+        console.log("Number of Attempts: ${guesses.length}\n");
+    } else{
+        console.log("YOU LOSE!\n");
+    }
+    
+    let cntn = prompt("Would you like to play again? (Y/N): ");
+    if(cntn.toUpperCase()=="Y"){
+        return game();
+    }
+    
+    console.log("exiting...\n")
+    return
+}
+
+game();
